7a6afc21475530a5742c464728b32ed1d4890e99
20213tn048 <86017217+Alberto89z@users.noreply.github.com>
2023-03-22 15:54:07 -0600

    responde servidor (me quiero cortar los huevos)

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,34 +1,64 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Text, ImageBackground } from "react-native";
-import { Formik } from "formik";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  ScrollView
+} from "react-native";
+import { Input, Button } from "react-native-elements";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 
-const SignupSchema = Yup.object().shape({
+const SignupSchema = Yup.object({
   email: Yup.string().email("Correo inválido").required("Campo obligatorio"),
   password: Yup.string().required("Campo obligatorio"),
   confirm: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Las contraseñas no coinciden")
+    .oneOf([Yup.ref("password")], "Las contraseñas no coinciden")
     .required("Campo obligatorio"),
   name: Yup.string().required("Campo obligatorio"),
   surname: Yup.string().required("Campo obligatorio"),
   code: Yup.string().required("Campo obligatorio"),
-  classroom: Yup.string().required("Campo obligatorio")
 });
 
 export default function RegisterScreen() {
   const [group, setGroup] = useState(null);
   const navigation = useNavigation();
-  const image = { uri: "https://p4.wallpaperbetter.com/wallpaper/141/158/403/simple-minimalism-gradient-wallpaper-preview.jpg" };
+  const image = {
+    uri: "https://p4.wallpaperbetter.com/wallpaper/141/158/403/simple-minimalism-gradient-wallpaper-preview.jpg",
+  };
 
-  const onSubmit = async (values, { setSubmitting }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirm:"",
+      name: "",
+      surname: "",
+      code: "",
+    },
+    validationSchema: SignupSchema,
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("Hola");
+      console.log(formValue)
+      onSubmite(formValue);
+    }
+  });
+
+  const onSubmite = async (values) => {
     try {
-      const response = await axios.post("http://192.168.34.248:8080/api-siblab/user/", values, {
-        headers: {
-          "Content-Type": "application/json"
+      const response = await axios.post(
+        "http://192.168.67.17:8080/api-siblab/user/",
+        values,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
+      );
       if (response.status === 200) {
         alert("Cuenta creada exitosamente");
         navigation.navigate("historial");
@@ -39,112 +69,63 @@ export default function RegisterScreen() {
       console.error("Error:", error);
       alert("Error al crear la cuenta");
     }
-    setSubmitting(false);
   };
 
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      </ImageBackground>
+      <ImageBackground
+        source={image}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
       <Text style={styles.title}>Registrar Cuenta</Text>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-          name: "",
-          surname: "",
-          role: "alumno",
-          code: "",
-          classroom: ""
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={onSubmit}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-          isSubmitting,
-        }) => (
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Nombre(s)"
-              onChangeText={handleChange("name")}
-              onBlur={handleBlur("name")}
-              value={values.name}
-            />
-            {errors.name && touched.name && (
-              <Text style={styles.error}>{errors.name}</Text>
-            )}
-            <TextInput
-              style={styles.input}
-              placeholder="Apellidos"
-              onChangeText={handleChange("surname")}
-              onBlur={handleBlur("surname")}
-              value={values.surname}
-            />
-            {errors.surname && touched.surname && (
-              <Text style={styles.error}>{errors.surname}</Text>
-            )}
-            <TextInput
-              style={styles.input}
-              placeholder="Matrícula"
-              onChangeText={handleChange("code")}
-              onBlur={handleBlur("code")}
-              value={values.code}
-            />
-            {errors.code && touched.code && (
-              <Text style={styles.error}>{errors.code}</Text>
-            )}
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              onChangeText={handleChange("email")}
-              onBlur={handleBlur("email")}
-              value={values.email}
-            />
-            {errors.email && touched.email && (
-              <Text style={styles.error}>{errors.email}
-
-
-              </Text>
-            )}
-            <TextInput
-              style={styles.input}
-              placeholder="Contraseña"
-              onChangeText={handleChange("password")}
-              onBlur={handleBlur("password")}
-              value={values.password}
-              secureTextEntry={true}
-            />
-            {errors.password && touched.password && (
-              <Text style={styles.error}>{errors.password}</Text>
-            )}
-            <TextInput
-              style={styles.input}
-              placeholder="Confirmar Contraseña"
-              onChangeText={handleChange("confirm")}
-              onBlur={handleBlur("confirm")}
-              value={values.confirm}
-              secureTextEntry={true}
-            />
-            {errors.confirm && touched.confirm && (
-              <Text style={styles.error}>{errors.confirm}</Text>
-            )}
-            <Button
-              title="Registrar"
-              onPress={handleSubmit}
-              disabled={isSubmitting}
-              buttonStyle={styles.button}
-            />
-          </View>
-        )}
-      </Formik>
+        <Input
+          containerStyle={styles.input}
+          placeholder="Nombre(s)"
+          onChangeText={(text) => formik.setFieldValue("name",text)}
+          errorMessage={formik.errors.name}
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Apellidos"
+          onChangeText={(text) => formik.setFieldValue("surname", text)}
+          errorMessage={formik.errors.surname}
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Matrícula"
+          onChangeText={(text) => formik.setFieldValue("code", text)}
+          errorMessage={formik.errors.code}
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => formik.setFieldValue("email", text)}
+          errorMessage={formik.errors.email}
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Contraseña"
+          onChangeText={(text) => formik.setFieldValue("password", text)}
+          errorMessage={formik.errors.password}
+          secureTextEntry={true}
+        />
+        <Input
+          containerStyle={styles.input}
+          placeholder="Confirmar Contraseña"
+          onChangeText={(text) => formik.setFieldValue("confirm", text)}
+          errorMessage={formik.errors.confirm}
+          secureTextEntry={true}
+        />
+        <Button
+          title="Registrar"
+          onPress={formik.handleSubmit}
+          loading={formik.isSubmitting}
+          buttonStyle={styles.button}
+        />
     </View>
+    </ScrollView>
   );
 }
 
@@ -175,16 +156,16 @@ const styles = StyleSheet.create({
     color: "red",
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     marginBottom: 20,
-    position: 'absolute',
+    position: "absolute",
   },
   button: {
     width: 100,
     height: 50,
     borderRadius: 10,
-    backgroundColor: "#black",
+    backgroundColor: "black",
     color: "#000",
     fontSize: 14,
     padding: 10,
